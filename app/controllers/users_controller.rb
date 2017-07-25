dacf2ae089d52b44ce7c8e3eab3c2a153c3b9fbf
taquanghoang <taquanghoang95@gmail.com>
2017-07-25 14:42:24 +0700

    activation account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,88 @@
-# Class users controller
 class UsersController < ApplicationController
-  def new; end
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+    :following, :followers]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = I18n.t("static_pages.check_mail")
+      redirect_to root_url
+      # Handle a successful save.
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = I18n.t("static_pages.profile_updated")
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = I18n.t("static_pages.user_deleted")
+    redirect_to users_url
+  end
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit :name, :email, :password,
+        :password_confirmation
+    end
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = I18n.t("static_pages.please_login")
+        redirect_to login_url
+      end
+    end
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
