03891f84e1c243e75f73f54d657f3a5da0ff3999
taquanghoang <taquanghoang95@gmail.com>
2017-07-25 14:39:41 +0700

    Add hello world!

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,18 @@
+# Static pages controller
+class StaticPagesController < ApplicationController
+  def home
+    puts 'home'
+  end
+
+  def help
+    puts 'help'
+  end
+
+  def about
+    puts 'about'
+  end
+
+  def contact
+    puts 'contact'
+  end
+end
